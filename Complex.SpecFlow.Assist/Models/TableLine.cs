55f267dc63ea1f5f797e7cbe5a820e399a2d7456
AndreVianna <andre.vianna.rj@hotmail.com>
2022-02-03 23:43:37 -0500

    Formalize Collection structure, rename and clean up

---
+++ Complex.SpecFlow.Assist/Models/TableLine.cs
@@ -0,0 +1,6 @@
+﻿namespace Complex.SpecFlow.Assist.Models;
+
+[ExcludeFromCodeCoverage]
+internal record TableLine(string Key, string Value) {
+    internal TableLine() : this(string.Empty, string.Empty) { }
+}
--- Complex.SpecFlow.Assist/Property.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Complex.SpecFlow.Assist;
-
-[ExcludeFromCodeCoverage]
-internal record Property(string Key, string Value, string Name, int[] Indexes, string[] Children);
